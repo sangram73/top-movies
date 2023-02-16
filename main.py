8784f4eb5b412f017d68bb40b5ea3f2fd5631117
Sangram <panda.sangram20@gmail.com>
2023-02-17 00:39:40 +0530

    creating database as movies

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 @app.route("/")
 def home():
