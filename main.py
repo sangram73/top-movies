1221c312eee0777ba69ce4ee9d7db27eab993f73
Sangram <panda.sangram20@gmail.com>
2023-02-17 00:48:31 +0530

    creating db table as movie

--- main.py
+++ main.py
@@ -10,10 +10,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+# create database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Create Table
+class Movie(db.Model):
+    id =db.Column(db.Integer,primary_key=True)
+    title =db.Column(db.String(250),unique=True,nullable=False)
+    Year =db.Column(db.Integer,nullable=False)
+    description =db.Column(db.String(500),nullable=False)
+    rating=db.Column(db.Float,nullable=True)
+    ranking =db.Column(db.Integer,nullable=True)
+    review=db.Column(db.String(250),nullable=True)
+    img=db.Column(db.String(250),nullable=False)
+db.create_all()
+    
+
+
+    
+
+    
 
 @app.route("/")
 def home():
