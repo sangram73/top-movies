ff503d2b8ffaac7d52e819b1e96cfd6ce30df857
Sangram <panda.sangram20@gmail.com>
2023-02-18 01:33:10 +0530

    adding some rwo in db

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -18,39 +18,41 @@ db = SQLAlchemy(app)
 # Create Table
 
 
-
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
+    title = db.Column(db.String(250), nullable=False)
     Year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(900), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
     rating = db.Column(db.Float, nullable=True)
     ranking = db.Column(db.Integer, nullable=True)
     review = db.Column(db.String(250), nullable=True)
     img = db.Column(db.String(250), nullable=False)
 
-with app.app_context():
-    db.create_all()
-# # After adding the new_movie the code needs to be commented out/deleted.
-# # So you are not trying to add the same movie twice.
+
+# After adding the new_movie the code needs to be commented out/deleted.
+# So you are not trying to add the same movie twice.
 # new_movie = Movie(
-#     title="Phone Booth",
-#     Year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     title="Avatar: The Way of Water ",
+#     Year=2022,
+#     description="Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.",
+#     rating=7.8,
+#     ranking=8,
+#     review="My favourite character was the sangram",
+#     img="https://m.media-amazon.com/images/M/MV5BYjhiNjBlODctY2ZiOC00YjVlLWFlNzAtNTVhNzM1YjI1NzMxXkEyXkFqcGdeQXVyMjQxNTE1MDA@._V1_FMjpg_UX1000_.jpg"
 # )
 # with app.app_context():
 #     # db.create_all()
 #     db.session.add(new_movie)
 #     db.session.commit()
 
+
 @app.route("/")
 def home():
-    all_movies=Movie.query.all()
-    return render_template("index.html",Movies=all_movies)
+
+    all_movies = db.session.query(Movie).all()
+    # all_movies = db.session.execute(Movie)
+    # all_movies= db.session.add(Movie)
+    return render_template("index.html", Movies=all_movies)
 
 
 if __name__ == '__main__':
