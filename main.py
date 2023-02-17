6ee37e131422acd48a25534be0fa2a909a866db8
Sangram <panda.sangram20@gmail.com>
2023-02-18 00:07:11 +0530

    push some data on it py help of py object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -11,31 +11,47 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
 # create database
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 # Create Table
+
+
+
 class Movie(db.Model):
-    id =db.Column(db.Integer,primary_key=True)
-    title =db.Column(db.String(250),unique=True,nullable=False)
-    Year =db.Column(db.Integer,nullable=False)
-    description =db.Column(db.String(500),nullable=False)
-    rating=db.Column(db.Float,nullable=True)
-    ranking =db.Column(db.Integer,nullable=True)
-    review=db.Column(db.String(250),nullable=True)
-    img=db.Column(db.String(250),nullable=False)
-db.create_all()
-    
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    Year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(900), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img = db.Column(db.String(250), nullable=False)
 
-
-    
-
-    
+with app.app_context():
+    db.create_all()
+# After adding the new_movie the code needs to be commented out/deleted.
+# So you are not trying to add the same movie twice.
+new_movie = Movie(
+    title="Phone Booth",
+    Year=2002,
+    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    rating=7.3,
+    ranking=10,
+    review="My favourite character was the caller.",
+    img="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+)
+with app.app_context():
+    # db.create_all()
+    db.session.add(new_movie)
+    db.session.commit()
+# db.create_all()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies=Movie.query.all()
+    return render_template("index.html",Movies=all_movies)
 
 
 if __name__ == '__main__':
